226e84f3bb98e087fa16a55bae066a933e1299aa
Jared Roesch <roeschinc@gmail.com>
2014-03-09 17:11:42 -0700

    add placeholder test cases

--- tests/test.hs
+++ tests/test.hs
@@ -1,9 +1,19 @@
 import Test.Tasty
+import Test.Tasty.HUnit as HU
+import Test.Tasty.QuickCheck as QC
+import Test.HUnit as H
 
-main :: IO ()
-main = defaultMain tests
+main = defaultMain $ testGroup "Tests" [qcProps]
 
-tests :: TestTree
-tests = testGroup "Tests" [unitTests]
+-- unitTests = testGroup "HUnit" [test_open]
 
-unitTests = testGroup "Unit Tests" []
+qcProps = testGroup "QuickCheck" [
+    property_sparse_dense_add, 
+    property_sparse_dense_mult, 
+    property_sparse_mult]
+
+property_sparse_dense_add = QC.testProperty "sparse + dense" (undefined :: String -> Bool)
+
+property_sparse_dense_mult = QC.testProperty "sparse * dense" (undefined :: String -> Bool)
+
+property_sparse_mult = QC.testProperty "sparse * sparse" (undefined :: String -> Bool)
