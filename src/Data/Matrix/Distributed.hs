50b6bf724f874b3e40463c8625e6f27940faf9f8
Jared Roesch <roeschinc@gmail.com>
2014-03-11 00:16:42 -0700

    finished battle with the compiler

--- src/Data/Matrix/Distributed.hs
+++ src/Data/Matrix/Distributed.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE FlexibleContexts #-}
-
 module Data.Matrix.Distributed where
 
+import Control.Applicative
+import Data.Matrix.Types
 import Data.Word
 import Data.Bits
 import Data.Vector as V
@@ -13,8 +14,9 @@ import Foreign.Storable
 import Numeric.Container
 import Control.Monad as CM
 import Control.Lens
-import Distribute (DMat, Distribute)
+import Distribute (Distribute)
 import qualified Distribute as DT
+import qualified Data.Serialize as Cereal
 
 {-
 --                 width height mat
@@ -47,8 +49,8 @@ matAdd m1@(SMat width1 height1 vm1) m2@(SMat width2 height2 vm2) =
     return $ SMat width1 height1 $ V.zipWith (+) vm1 vm2
 -}
 
-sdMul :: (Num a, S.Eq0 a, Storable a) => S.Mat a -> D.Matrix a -> S.Mat a
-sdMul a b = a & S._Mat %~ H.map (\(S.Key r c, d) -> (S.Key r c, d * (b @@> (fromIntegral r, fromIntegral c))))
+sdMult :: (Num a, S.Eq0 a, Storable a) => S.Mat a -> D.Matrix a -> S.Mat a
+sdMult a b = a & S._Mat %~ H.map (\(S.Key r c, d) -> (S.Key r c, d * (b @@> (fromIntegral r, fromIntegral c))))
 
 -- TODO: am i accidentaly transposing
 sdAdd :: (Num a, S.Eq0 a, Storable a) => S.Mat a -> D.Matrix a -> D.Matrix a
@@ -69,86 +71,81 @@ sdAdd a b = (height><width) $ V.toList resV
       write mv (cc + rr * width) (val + vals)
       return ()
 
--- sparse matrix based on peano ordering
---                  top left   top right  bottom left bottom right
-data DMat a = Node !Int !(DMat a) !(DMat a) !(DMat a) !(DMat a)
-            | Remote DT.PID
-            | Dense (D.Matrix a)
-            | Sparse (S.Mat a)
-            | Zero
-            deriving (Show)
-
-{- sadd :: (S.Eq0 a, Num a, Container Matrix a) => DMat a -> DMat a -> DMat a
-sadd a b = case (a, b) of
-                (Zero, r) -> r
-                (l, Zero) -> l
-                (Sparse left, Sparse right) -> Sparse $ left + right
-                (Dense left, Dense right) -> Dense $ left `add` right
-                (Node _ l1 l2 l3 l4, Node _ r1 r2 r3 r4) -> Node (sadd l1 r1)
-                                                             (sadd l2 r2)
-                                                             (sadd l3 r3)
-                                                             (sadd l4 r4)
-                (Sparse left, Dense right) -> Dense $ sdAdd left right
-                (Dense right, Sparse left) -> Dense $ sdAdd left right
-                otherwise -> Zero -}
+sadd :: (S.Eq0 a, Num a, Container Matrix a) => CMat a -> CMat a -> CMat a
+sadd Zero r = r
+sadd l Zero = l
+sadd (Sparse left) (Sparse right) =
+    Sparse $ left + right
+sadd (Dense left) (Dense right) =
+    Dense $ left `add` right
+sadd (Sparse left) (Dense right) =
+    Dense $ sdAdd left right
+sadd (Dense right) (Sparse left) =
+    Dense $ sdAdd left right
 
 
-{- smult :: (S.Eq0 a, Num a, Product a, Container Matrix a) => DMat a -> DMat a -> DMat a
-smult a b = case (a, b) of
-                (Zero, _) -> Zero
-                (_, Zero) -> Zero
-                (Sparse left, Sparse right) -> Sparse $ left * right
-                (Dense left, Dense right) -> Dense $ left `multiply` right
-                (Node l1 l2 l3 l4, Node r1 r2 r3 r4) -> Node (sadd (smult l1 r1) (smult l2 r3))
-                                                             (sadd (smult l1 r2) (smult l2 r4))
-                                                             (sadd (smult l3 r1) (smult l4 r3))
-                                                             (sadd (smult l3 r2) (smult l4 r4))
-                (Sparse left, Dense right) -> Sparse $ sdMul left right
-                (Dense left, Sparse right) -> Sparse $ (flip sdMul) left right
-                otherwise -> Zero -- TODO: fix -}
+smult :: (S.Eq0 a, Num a, Product a, Container Matrix a) => CMat a -> CMat a -> CMat a
+smult Zero _ = Zero
+smult _ Zero = Zero
+smult (Sparse left) (Sparse right) =
+  Sparse $ left * right
+smult (Dense left) (Dense right) =
+  Dense $ left `multiply` right
+smult (Sparse left) (Dense right) =
+  Sparse $ sdMult left right
+smult (Dense left) (Sparse right) =
+  Sparse $ (flip sdMult) left right
 
--- current assumption that the matrix is square is bad
-
-fromSparse :: S.Mat a -> DMat a
+fromSparse :: [a] -> DMat a
 fromSparse = undefined
 
-dmult :: DMat a -> DMat a -> Distribute (DMat a) DMat a
-dmult (Node m1 tl1 tr1 bl1 br1) (Node m2 tl2 tr2 bl2 br2) = do
-  when ((m1 .&. m2) /= 0) error "Wrong" -- assert this
-  nodeMult m1 tl1 tr1 bl1 br1 tl2 tr2 bl2 br2
-dmult (Node _ _ _ _ _) _ =
-  error "Internal error: fuck andrew berls"
-dmult _ (Node _ _ _ _ _) =
-  error "Internal error: fuck pete cruz"
-dmult Zero _ =
-  Zero
-dmult _ Zero =
-  Zero
-dmult (Remote pid) mat =
-    remoteMult pid mat
-dmult mat (Remote pid) =
-    remoteMult pid mat
-dmult (Sparse smat) (Dense dmat) =
-    return $ Sparse (sdMult smat dmat)
-dmult (Dense dmat) (Sparse smat) =
-    return $ Sparse (sdMult smat dmat)
-    remoteMult pid mat
-  where remoteMult pid mat = do
-          requestMatrix
-          rmat <- DT.readFrom pid
-          dmult rmat mat
+requestMatrix = undefined
 
-        nodeMult tl tr bl br tl' tr' bl' br' = do
-          topLeft <- applyOrId (firstQ mask) tl' (dmult tl)
-          topRight <- applyOrId (secondQ mask) tr' (dmult tr)
-          bottomLeft <- applyOrId (thirdQ mask) bl' (dmult bl)
-          bottomRight <- applyOrId (fourthQ mask) br' (dmult br)
-          return (Node mask topLeft topRight bottomLeft bottomRight)
+dadd :: (Num a, S.Eq0 a, Storable a, Container Matrix a) => DMat a -> DMat a -> Distribute (DMatMessage a) (DMat a)
+dadd (Concrete cmat1) (Concrete cmat2) =
+    return $ Concrete $ sadd cmat1 cmat2
+dadd (Remote pid) mat = do
+    rmat <- requestMatrix
+    dadd rmat mat
+dadd mat (Remote pid) = do
+    rmat <- requestMatrix
+    dadd mat rmat
+dadd (DMat m1 l1 l2 l3 l4) (DMat m2 r1 r2 r3 r4) = do
+  let mask = m1 .&. m2
+  tl <- ternary (firstQ mask) (return l1) (dadd l1 r1)
+  tr <- ternary (secondQ mask) (return l2) (dadd l2 r2)
+  bl <- ternary (thirdQ mask) (return l3) (dadd l3 r3)
+  br <- ternary (fourthQ mask) (return l4) (dadd l4 r4)
+  return $ DMat mask tl tr bl br
 
-        applyOrId False a _ = a
-        applyOrId True  a f = f a
+dmult :: (Num a, S.Eq0 a, Storable a, Container Matrix a, Product a) =>
+      DMat a -> DMat a -> Distribute (DMatMessage a) (DMat a)
+dmult (Concrete cmat1) (Concrete cmat2) =
+    return $ Concrete $ smult cmat1 cmat2
+dmult (Remote pid) mat = do
+    rmat <- requestMatrix
+    dmult rmat mat
+dmult mat (Remote pid) = do
+    rmat <- requestMatrix
+    dmult mat rmat
+dmult (DMat m1 l1 l2 l3 l4) (DMat m2 r1 r2 r3 r4) = do
+    let mask = m1 .&. m2
+    topLeft <- ternary (firstQ mask) (return l1) $
+      CM.join $ (liftM2 dadd) (dmult l1 r1) (dmult l2 r3)
+    topRight <- ternary (secondQ mask) (return l2) $
+      CM.join $ (liftM2 dadd) (dmult l1 r2) (dmult l2 r4)
+    bottomLeft <- ternary (thirdQ mask) (return l3) $
+      CM.join $ (liftM2 dadd) (dmult l3 r1) (dmult l4 r3)
+    bottomRight <- ternary (fourthQ mask) (return l4) $
+      CM.join $ (liftM2 dadd) (dmult l3 r2) (dmult l4 r4)
+    return $ DMat mask topLeft topRight bottomLeft bottomRight
+dmult _ _ = error "undefined behavior"
 
-        firstQ m = (m .&. 1) == 1
-        secondQ m = (m .&. 13) == 1
-        thirdQ m = (m .&. 11) == 1
-        fourthQ m = (m .&. 8) == 1
+ternary :: Bool -> r -> r -> r
+ternary False a _ = a
+ternary True  _ b = b
+
+firstQ m = (m .&. 1) == 1
+secondQ m = (m .&. 13) == 1
+thirdQ m = (m .&. 11) == 1
+fourthQ m = (m .&. 8) == 1
---
+++ src/Data/Matrix/Types.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE DeriveGeneric, DefaultSignatures, TypeSynonymInstances, FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies, UndecidableInstances, OverlappingInstances, ScopedTypeVariables #-}
+module Data.Matrix.Types
+  ( DMat(..)
+  , CMat(..)
+  , DMatMessage(..)
+  ) where
+
+import Distribute as DT
+import qualified Sparse.Matrix as S
+import qualified Sparse.Matrix.Internal.Array as SI
+import Data.Functor
+import qualified Data.Packed as D
+import qualified Data.Packed.ST as DS
+import Data.Packed.Development (MatrixOrder(..))
+import qualified Data.Serialize as Cereal
+import qualified Data.Vector.Serialize
+import Foreign.Storable
+import Control.Monad
+import Control.Monad.ST
+import GHC.Generics
+
+{- instance Cereal.Serialize a => (Cereal.Serialize (S.Arr a a)) where
+  get = undefined
+  put = undefined -}
+
+instance ((S.Arr a ~ array), Cereal.Serialize a)  => (Cereal.Serialize (array a)) where
+    get = Cereal.get :: Cereal.Get (array a)
+    put = Cereal.put :: Cereal.Putter (array a)
+
+-- This really shouldn't type check this some hacky shit
+instance (SI.Arr a ~ array, Cereal.Serialize a) => (Cereal.Serialize (S.Mat a)) where
+    get = do
+      size <- Cereal.get
+      k1 <- Cereal.get
+      k2 <- Cereal.get
+      content <- Cereal.get :: Cereal.Get (array a)
+      return $ S.Mat size k1 k2 content
+
+    put (S.Mat size k1 k2 content) = do
+      Cereal.put size
+      Cereal.put k1
+      Cereal.put k2
+      Cereal.put (content :: array a)
+
+instance (Cereal.Serialize a, D.Element a) => (Cereal.Serialize (D.Matrix a)) where
+    -- can most likely get better performance here with unsafeIO and
+    -- unsafeStToIO
+    get = do
+      r <- Cereal.get :: Cereal.Get Int
+      c <- Cereal.get :: Cereal.Get Int
+      values <- forM [0..r] $ (\_ ->
+                  forM [0..c] $ \_ -> Cereal.get)
+      return $ D.fromLists values
+
+    -- Make sure iterations happen in the row/column major order
+    put mat = runST $ do
+        let r = D.rows mat
+        let c = D.cols mat
+        mmat <- DS.thawMatrix mat
+        puts <- concat <$> forM [0..r] (\i ->
+                  forM [0..c] $ \j -> do
+                    value <- DS.readMatrix mmat i j
+                    return $ Cereal.put value)
+        DS.freezeMatrix mmat
+        return $ foldl (>>) (Cereal.put r >> Cereal.put c) puts
+
+-- sparse matrix based on peano ordering
+--                      top left  top right bottom left bottom right
+
+-- | A concrete matrix either dense, or sparse, zero
+data CMat a = Dense (D.Matrix a)
+            | Sparse (S.Mat a)
+            | Zero
+            deriving (Show, Generic)
+
+instance (Cereal.Serialize a, D.Element a)  => (Cereal.Serialize (CMat a))
+
+-- | A distributed matrix
+data DMat a = DMat !Int !(DMat a) !(DMat a) !(DMat a) !(DMat a)
+            | Remote DT.PID
+            | Concrete (CMat a)
+            deriving (Show, Generic)
+
+instance (Cereal.Serialize a, D.Element a)  => (Cereal.Serialize (DMat a))
+
+data DMatMessage a = Request (DMat a)
+                   | Response (DMat a)
+                   deriving (Generic)
+
+instance (Cereal.Serialize a, D.Element a) => (Cereal.Serialize (DMatMessage a))
