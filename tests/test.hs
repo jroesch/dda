49b9dd96156f47064c627058800bff092d0ea339
Tristan Konolige <tristan.konolige@gmail.com>
2014-03-11 00:48:05 -0700

    distributed multiplication test case

--- tests/test.hs
+++ tests/test.hs
@@ -3,9 +3,15 @@ import Test.Tasty.HUnit as HU
 import Test.Tasty.QuickCheck as QC
 import Test.HUnit as H
 
+import Data.Matrix.Types
+import Data.Matrix.Distributed
+import qualified Data.Packed as D
+import Numeric.Container
+import qualified Sparse.Matrix as S
+
 main = defaultMain $ testGroup "Tests" [qcProps]
 
--- unitTests = testGroup "HUnit" [test_open]
+unitTests = testGroup "HUnit" [test_local_mult]
 
 qcProps = testGroup "QuickCheck" [
     property_sparse_dense_add, 
@@ -17,3 +23,11 @@ property_sparse_dense_add = QC.testProperty "sparse + dense" (undefined :: Strin
 property_sparse_dense_mult = QC.testProperty "sparse * dense" (undefined :: String -> Bool)
 
 property_sparse_mult = QC.testProperty "sparse * sparse" (undefined :: String -> Bool)
+
+test_local_mult = HU.testCase "multiplication of two identitty matrices should yield and identity matrix" test
+  where
+    test = do
+      let mat = DMat 3 (Concrete (Dense (ident 8))) (Concrete Zero) (Concrete Zero) (Concrete (Sparse (S.ident 8)))
+      let mat' = DMat 3 mat (Concrete Zero) (Concrete Zero) mat
+      let result = dmult mat' mat'
+      assertEqual "expecting identity" mat' result
