b229fec1f55966bc13ae6ad763b883dd6b3c4c09
Tristan Konolige <tristan.konolige@gmail.com>
2014-03-17 11:22:49 -0700

    builder can make a matrix for 4^n nodes

--- src/Data/Matrix/Distributed/Builder.hs
+++ src/Data/Matrix/Distributed/Builder.hs
@@ -5,15 +5,18 @@ import Data.Matrix.Distributed.Types
 import qualified Sparse.Matrix as S
 import Data.Bits
 
+constructMat :: MElement a => Int -> Int -> Int -> DMat a
+constructMat s id n = constructMat' (fromIntegral s) (fromIntegral id) (fromIntegral n) 0
 
 -- construct a matrix of size s x s
-constructMat s id n = mat
+constructMat'' s id n off = mat
   where
     mat = case id of
-            0 -> DMat 1 (Concrete $ Sparse $ S.fromList list) (Remote 1 [B]) (Remote 2 [C]) (Remote 3 [D])
-            1 -> DMat 2 (Remote 0 [A]) (Concrete $ Sparse $ S.fromList list) (Remote 2 [C]) (Remote 3 [D])
-            2 -> DMat 4 (Remote 0 [A]) (Remote 1 [B]) (Concrete $ Sparse $ S.fromList list) (Remote 3 [D])
-            3 -> DMat 8 (Remote 0 [A]) (Remote 1 [B]) (Remote 2 [C]) (Concrete $ Sparse $ S.fromList list)
+            0 -> DMat 1 (Concrete $ Sparse $ S.fromList list) (Remote (off + 1) [B]) (Remote (off' + 2) [C]) (Remote (off' + 3) [D])
+            1 -> DMat 2 (Remote (off' + 0) [A]) (Concrete $ Sparse $ S.fromList list) (Remote (off' + 2) [C]) (Remote (off' + 3) [D])
+            2 -> DMat 4 (Remote (off' + 0) [A]) (Remote (off' + 1) [B]) (Concrete $ Sparse $ S.fromList list) (Remote (off' + 3) [D])
+            3 -> DMat 8 (Remote (off' + 0) [A]) (Remote (off' + 1) [B]) (Remote (off' + 2) [C]) (Concrete $ Sparse $ S.fromList list)
+    off' = fromIntegral off
     n' = floor $ sqrt $ fromIntegral n
     ix = id `mod` n'
     iy = id `div` n'
@@ -26,6 +29,22 @@ constructMat s id n = mat
     bot  x xs = if x `div` s' >= s'-1 then xs else (S.Key (x+s') x, -1):xs
     top  x xs = if x `div` s' < 1 then xs else (S.Key (x-s') x, -1):xs
     right x xs = if x `mod` s' >= s'-1 then xs else (S.Key (x+1) x, -1):xs
+
+constructMat' s id n off = case n of 
+                            4 | id >= off && id < off + n' -> constructMat'' s (id-(fromIntegral off)) n (fromIntegral off)
+                            4 -> (Remote ((fromIntegral off) + (fromIntegral id)) quad)
+                            _ -> DMat 15 (constructMat' s id n' 0)
+                                         (constructMat' s id n' n')
+                                         (constructMat' s id n' (2*n'))
+                                         (constructMat' s id n' (3*n'))
+                            
+  where
+    n' = n `div` 4
+    quad = case id of
+             0 -> [A]
+             1 -> [B]
+             2 -> [C]
+             3 -> [D]
 
 {-
 constructMat s id n l off = case n of
