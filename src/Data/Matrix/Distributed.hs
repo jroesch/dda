e90600acce7fac2e7e8719b7de57a07bc679ec02
Tristan Konolige <tristan.konolige@gmail.com>
2014-03-17 22:10:21 -0700

    working

--- src/Data/Matrix/Distributed.hs
+++ src/Data/Matrix/Distributed.hs
@@ -30,8 +30,9 @@ compute pid action = do
 topleft :: (MElement a) => DMat a -> Distribute (DMatMessage a) a
 topleft m = sync (m, m) (unsafeTopLeft m)
   where
-    unsafeTopLeft (Concrete (Dense smat)) = undefined -- return $ smat @@> (0,0)
-    unsafeTopLeft (Concrete (Sparse smat)) = undefined --TODO: TK CHECK OUT send to all here
+    unsafeTopLeft (Concrete (Dense smat)) = return $ smat @@> (0,0)
+    unsafeTopLeft (Concrete (Sparse smat)) = let list = H.filter (\(S.Key a b, _) -> a == 0 && b == 0) $ (from S._Mat) # smat
+                                              in return $ if empty list then 0 else snd $ list H.! 0
     unsafeTopLeft (Concrete Zero) = return 0
     unsafeTopLeft r @ (Remote pid quad) = do
         mat <- requestMatrix pid L quad
