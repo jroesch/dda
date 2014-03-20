1c2848c449ab2c04681ec607fc7a6272e29b2857
Tristan Konolige <tristan.konolige@gmail.com>
2014-03-19 23:22:35 -0700

    timing

--- triton/matBench.hs
+++ triton/matBench.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, BangPatterns #-}
 import Data.Matrix.Distributed.Types
 import Data.Matrix.Distributed.Builder
 import Data.Matrix.Distributed
 
 import System.Environment
 import System.IO
+import System.TimeIt
 import Data.Functor
 import Data.List.Split
 import Control.Monad.Trans
@@ -24,13 +25,11 @@ main = do
         procs = zip3 [0..] nodes (repeat 3000)
 
     putStrLn "Starting up"
-    compute rank procs $ do
-      let n   = 4
-          mat :: DMat Double = constructMat 4 rank size
+    n:xs <- getArgs
+    timeIt $ compute rank procs $ do
+      let mat :: DMat Double = constructMat (read n) rank size
       lift $ print $ show rank ++ " " ++ show mat
-      m' <- mat .* mat
-      lift $ print "----------------"
-      lift $ print m'
+      !m' <- mat .* mat
       return ()
     putStrLn "DONE __________"
     return ()
